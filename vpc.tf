d1f5961d2fd80346298b823d8b0e3088a1309f21
t <tezar@talk21.com>
2021-11-04 12:39:45 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,14 +1,14 @@
-module "vpc" {
-    source  = "terraform-google-modules/network/google"
-    version = "~> 3.0"
-    network_name = "gaurav-network"
-    project_id = var.project
+module "network" {
+  source  = "app.terraform.io/gcpworkshopnov/network/google"
+  version = "3.4.0"
+  network_name = "gaurav-network"
+  project_id = var.project
 
-    subnets = [
-      {
-        subnet_name   = "gaurav-subnet"
-        subnet_ip     = "10.100.10.0/24"
-        subnet_region = var.region
-      }
-    ]
+  subnets = [
+  {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+  ]
 }
